e2cf0c47089cab9704f398bae213892e726f3742
yuki <yukihiro@ubisam.com>
2024-04-18 22:56:10 +0900

    Navigator view layout comp

---
+++ layout-navigetion-app/Layouts/MainView/MainScreen.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './ScreenView/HomeScreen';
+import NextScreen from './ScreenView/NextScreen';
+import SettingsScreen from './ScreenView/SettingsScreen';
+import TabBar from '../TabBar/TabBar';
+
+const Tab = createBottomTabNavigator();
+
+const MainScreen = () => {
+    return (
+        <Tab.Navigator tabBar={(props) => <TabBar {...props} />}>
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Next" component={NextScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+    );
+};
+
+
+export default MainScreen;
---
+++ layout-navigetion-app/README.md
@@ -0,0 +1,22 @@
+expo 설치
+npm install -g expo-cli
+
+reactnative 설치 
+npx create-expo-app 제목
+
+가상 핸들폰에 실행
+npx expo start
+
+npm install react-native-webview
+
+먼저 종속성과 함께 @react-navigation/native 패키지를 설치해야 합니다.
+npm install @react-navigation/native
+
+React Navigation에는 또한 직접 설치해야 하는 몇 가지 피어 종속성이 필요합니다
+npm install react-native-screens react-native-safe-area-context
+
+네이티브 스택 탐색기를 사용하는 경우 @react-navigation/native-stack을 설치해야 합니다.
+npm install @react-navigation/native-stack
+
+하단 탭 탐색기를 사용하려면 @react-navigation/bottom-tabs를 설치해야 합니다.
+npm install @react-navigation/bottom-tabs
