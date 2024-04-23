8b2d04a177929261d66a7d741f8dfdf287f2288b
yuki <yukihiro@ubisam.com>
2024-04-23 23:05:01 +0900

    기본 구성 완료

--- uibisam-app-project/App.js
+++ uibisam-app-project/App.js
@@ -3,6 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import UbisamSplashScreen from './Ubisam_Splash';
 
+import LogInView from './Component/UnitLogin/LogInView';
+import MainScreen from './Component/Layouts/MainView/MainScreen';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +14,14 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
         <Stack.Screen name="Splash" component={UbisamSplashScreen} options={{ headerShown: false }} />
-        {/* <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen name="Login" component={LogInView} options={{ headerShown: false }} />
+        <Stack.Screen 
+          name="Ubisam" 
+          component={MainScreen} 
+          options={{
+            headerShown: false, // Do not show the header
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
